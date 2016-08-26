796d06fab87411552e75693d5ee2c779c05620fb
alanzablocki <alan.zablocki@gmail.com>
2016-08-26 00:05:03 +0000

    moment of truth

--- app.py
+++ app.py
@@ -22,11 +22,43 @@ def index():
         print name
 
         if form.validate():
-	    return redirect('/success')
+	    #return redirect('/success')
+	    return redirect('/graph')
             #flash('Thanks for registration ' + name)
         else:
             flash('Error: All the form fields are required. ')
     return render_template('index.html', form=form)
+
+@app.route('/graph',methods=['GET','POST']) # might have to add some stuff here!
+def graph():
+    stock_name = "AAPL" #name
+    # 2 get stock api
+    url = 'https://www.quandl.com/api/v1/datasets/WIKI/%s.json' % stock_name
+    # select dates
+    # get the data
+
+    # from tdi blog, slightly different than p171 in Pandas Wes McKinney
+    session = requests.Session()
+    session.mount('http://', requests.adapters.HTTPAdapter(max_retries=3))
+    raw_data = session.get(url)
+
+    session = requests.Session()
+    session.mount('http://', requests.adapters.HTTPAdapter(max_retries=3))
+    raw_data = session.get(url)
+    # json decode
+    rd = raw_data.json()
+
+    # create pandas data frame
+    df = pd.DataFrame(rd['data'],columns=rd['column_names'])
+    df = df.set_index('Date')
+    df.index = pd.to_datetime(df.index)
+
+    p = figure(width=400, height=300, x_axis_type="datetime",x_axis_label="Date",title=stock_name + " Stock")
+    p.line(df.index, df['Open'], color='green', legend='Opening Price')
+    
+    script, div = components(p)
+    return render_template('graph.html', script=script, div=div, form=form) # added form=form
+
 
 @app.route('/success')
 def success():
